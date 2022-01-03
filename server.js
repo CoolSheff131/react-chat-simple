b19b12a6700b8097e65cce322607b1b4027f98b5
Александр Никульшеев <a.nikulsheev@gmail.com>
2022-01-03 21:00:57 +0800

    Обработка ошибки запуска сервера

--- server.js
+++ server.js
@@ -2,8 +2,17 @@ const express = require('express');
 
 const app = express();
 
+const rooms = {
+  rooms: [],
+};
+
 app.get('/users', function (req, res) {
   res.send('ss');
 });
 
-app.listen(5000);
+app.listen(5000, (err) => {
+  if (err) {
+    throw Error(err);
+  }
+  console.log('Started');
+});
